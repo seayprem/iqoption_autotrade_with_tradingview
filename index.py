d3308ac34a597f4a6a80e25161749954de0480b9
SeayPrem <seayprem@hotmail.com>
2021-02-10 00:26:09 -0800

    Second Commit

--- index.py
+++ index.py
@@ -1,2 +1,24 @@
+from iqoptionapi.stable_api import IQ_Option
+
+
+
+# Login
+def signIn(username, password):
+    global API = IQ_Option(username, password)
+    global API.connect()
+    return True
+
+
+# Mode Real Money & PRATICE Money
+# You can change this here.
+def changeModeBalance():
+    pass
+
+
+# Tracking Signal StrongBuy Buy Nature Sell StrongSell
+def trackingSignal():
+    pass
+
+# Main Process
 if __name__ == "__main__":
-    print("HELLO WORLD");
+    pass
